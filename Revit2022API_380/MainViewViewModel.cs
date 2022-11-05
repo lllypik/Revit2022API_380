527e50e090b3b8b86a2e57a074ba19d7d29c4810
lllypik <blesphoton@gmail.com>
2022-11-05 21:09:54 +0300

    Добавьте файлы проекта.

---
+++ Revit2022API_380/MainViewViewModel.cs
@@ -0,0 +1,225 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Revit2022API_380
+{
+    class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand ConvertDWGCommand { get; }
+        public DelegateCommand PrintCommand { get; }
+        public DelegateCommand ConvertIFCCommand { get; }
+        public DelegateCommand ConvertNWCCommand { get; }
+        public DelegateCommand ConvertJPEGCommand { get; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            ConvertDWGCommand = new DelegateCommand(OnConvertDWGCommand);
+            PrintCommand = new DelegateCommand(OnPrintCommand);
+            ConvertIFCCommand = new DelegateCommand(OnConvertIFCCommand);
+            ConvertNWCCommand = new DelegateCommand(OnConvertNWCCommand);
+            ConvertJPEGCommand = new DelegateCommand(OnConvertJPEGCommand);
+        }
+
+        private void OnConvertJPEGCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            using (Transaction transaction = new Transaction(doc, "export IFC"))
+            {
+                transaction.Start();
+
+                View view = doc.ActiveView;
+
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), view.Name);
+
+
+                ImageExportOptions imageExportOptions = new ImageExportOptions();
+
+                 imageExportOptions.ZoomType = ZoomFitType.FitToPage;
+
+                 imageExportOptions.PixelSize = 1920;
+
+                 imageExportOptions.FitDirection = FitDirectionType.Horizontal;
+
+                 imageExportOptions.ExportRange = ExportRange.CurrentView;
+
+                 imageExportOptions.HLRandWFViewsFileType = ImageFileType.JPEGLossless;
+
+                 imageExportOptions.FilePath = filePath;
+
+                 imageExportOptions.ImageResolution = ImageResolution.DPI_600;
+
+                 imageExportOptions.ShadowViewsFileType = ImageFileType.JPEGLossless;
+
+
+                doc.ExportImage(imageExportOptions);
+
+                transaction.Commit();
+            }
+        }
+
+        private void OnConvertNWCCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            using (Transaction transaction = new Transaction(doc, "export IFC"))
+            {
+                transaction.Start();
+
+                NavisworksExportOptions nVCExportOptions = new NavisworksExportOptions();
+
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Export.ifc", nVCExportOptions);
+
+                transaction.Commit();
+            }
+        }
+
+        private void OnConvertIFCCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            using (Transaction transaction = new Transaction(doc, "export IFC"))
+            {
+                transaction.Start();
+
+                IFCExportOptions iFCExportOptions = new IFCExportOptions();
+
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Export.ifc", iFCExportOptions);
+
+                transaction.Commit();
+            }
+        }
+
+        private void OnConvertDWGCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            using (Transaction transaction = new Transaction(doc, "export DWG"))
+            {
+                transaction.Start();
+
+                ViewPlan viewPlan = new FilteredElementCollector(doc)
+                                          .OfClass(typeof(ViewPlan))
+                                          .Cast<ViewPlan>()
+                                          .FirstOrDefault(v => v.ViewType == ViewType.FloorPlan &&
+                                                                             v.Name.Equals("Level 1"));
+                DWGExportOptions dwgOption = new DWGExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                                                     "export.dwg",
+                                                     new List<ElementId> { viewPlan.Id },
+                                                     dwgOption);
+
+                transaction.Commit();
+            }
+        }
+
+        private void OnPrintCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<ViewSheet> sheets = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(ViewSheet))
+                .Cast<ViewSheet>()
+                .ToList();
+
+            var groupedSheets = sheets.GroupBy(sheet => doc.GetElement(new FilteredElementCollector(doc, sheet.Id)
+                                                  .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                                                  .FirstElementId()).Name);
+
+            List<ViewSet> viewSets = new List<ViewSet>();
+
+            PrintManager printManager = doc.PrintManager;
+
+            printManager.SelectNewPrintDriver("PDFCreator");
+
+            printManager.PrintRange = PrintRange.Select;
+
+            ViewSheetSetting viewSheetSetting = printManager.ViewSheetSetting;
+
+            foreach (var groupedSheet in groupedSheets)
+            {
+                if (groupedSheet.Key == null)
+                    continue;
+
+                ViewSet viewSet = new ViewSet();
+
+                List<ViewSheet> sheetsofGroup = groupedSheet.Select(s => s).ToList();
+
+                foreach (var sheet in sheetsofGroup)
+                {
+                    viewSet.Insert(sheet);
+                }
+
+                viewSets.Add(viewSet);
+
+                printManager.PrintRange = PrintRange.Select;
+
+                viewSheetSetting.CurrentViewSheetSet.Views = viewSet;
+
+                using (Transaction transaction = new Transaction(doc, "Create view set"))
+                {
+                    transaction.Start();
+
+                    viewSheetSetting.SaveAs($"{groupedSheet.Key}_{Guid.NewGuid()}");
+
+                    transaction.Commit();
+                }
+
+                bool isFormatSelected = false;
+
+                foreach (PaperSize paperSize in printManager.PaperSizes)
+                {
+                    if (string.Equals(groupedSheet.Key, "А4К") &&
+                        string.Equals(paperSize.Name, "A4"))
+
+                    {
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PaperSize = paperSize;
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PageOrientation = PageOrientationType.Portrait;
+                        isFormatSelected = true;
+                    }
+                    else if (string.Equals(groupedSheet.Key, "А3А") &&
+                        string.Equals(paperSize.Name, "A3"))
+
+                    {
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PaperSize = paperSize;
+                        printManager.PrintSetup.CurrentPrintSetting.PrintParameters.PageOrientation = PageOrientationType.Landscape;
+                        isFormatSelected = true;
+                    }
+                }
+
+                if(isFormatSelected = false)
+                {
+                    TaskDialog.Show("Ошибка", "Не найден формат");
+                }
+
+                printManager.CombinedFile = false;
+                printManager.SubmitPrint();
+            }
+        }
+
+        public event EventHandler CloseReqest;
+        private void RaiseCloseReqest()
+        {
+            CloseReqest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Revit2022API_380/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Revit2022API_380")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Revit2022API_380")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a7357be9-a093-46ee-9ad5-d8163dc90960")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
